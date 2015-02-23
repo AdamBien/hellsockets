510bd4b08b79320ba5208342f843dde45c8a6a99
Kristian Frølund Jørgensen <kristian@tlt.local>
2015-02-23 23:16:54 +0400

    Added angularjs application

---
+++ hellsockets/src/main/webapp/res/js/services.js
@@ -0,0 +1,15 @@
+angular.module('hellsockets')
+.factory('Loader', ['$http', function($http){
+    return {
+        start: function(config){
+            return $http.post('resources/loader', config)
+            .then(function(result){
+                if(result.status == 202){
+                    return {status: 'Started'}
+                }else{
+                    return {status: 'Not started'}
+                }
+            });
+        }
+    };
+}]);
